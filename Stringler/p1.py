dd39c7c8316ed88b6814c088b9252bb6765b2975
Selim Özbay <selimozbay@yandex.com>
2024-04-04 23:03:08 +0300

    Create p1.py
    Created alternative solution for problem 1.

---
+++ Stringler/p1.py
@@ -0,0 +1,9 @@
+while True:
+
+    girilenKelime = input("\nKelime Giriniz\n")
+
+
+    if girilenKelime == girilenKelime.lower():
+        print("Girilen kelimeler küçük harften oluşur.\n")
+    else:
+        print("Girilen kelimede büyük harfler bulunuyor.\n")
